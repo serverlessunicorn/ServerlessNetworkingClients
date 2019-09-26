8afb43bf02d53d27a7eec8f1eef92dfac17509d7
Tim Wagner <timwagner@Tims-MacBook-Pro.local>
2019-09-26 12:04:21 -0700

    Restructure and rename tests to get SAM build and publish working. Still a bit of a work in progress here; next up is getting SAR publish to work.

--- aws/python/tests/lambda_config_test.py
+++
@@ -1,5 +0,0 @@
-# Basic test - verifies that UDT-containing layer and LD_LIBRARY_PATH are set up correctly.
-import udt4py
-def lambda_handler(event, context):
-    sock = UDTSocket()
-    return {'statusCode': 200, 'body': 'Configuration test completed successfully'}
